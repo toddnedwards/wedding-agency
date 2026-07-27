4ada98b3bfe671cbf322edd97b71eddac32dd07f
Todd Edwards <138999735+toddnedwards@users.noreply.github.com>
2026-07-27 18:51:17 +0100

    Add main project URL configuration

---
+++ wedding_agency/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('apps.accounts.urls')),
+    path('bookings/', include('apps.bookings.urls')),
+    path('vendors/', include('apps.vendors.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
