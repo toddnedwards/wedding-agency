2a23fbbbef8004128cb491fc329dbe2e859cdfb3
Todd Edwards <138999735+toddnedwards@users.noreply.github.com>
2026-07-27 18:21:02 +0100

    Add core app URLs

---
+++ apps/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('services/', views.ServicesView.as_view(), name='services'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('blog/', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
+]
